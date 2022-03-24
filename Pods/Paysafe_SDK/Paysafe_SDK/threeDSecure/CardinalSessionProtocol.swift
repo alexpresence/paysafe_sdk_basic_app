8a79514a50933989bca4277b1dfbb28e37046dbd
Alexander Murphy <amurphy345@gmail.com>
2022-03-24 08:41:32 -0600

    check in pod

---
+++ Pods/Paysafe_SDK/Paysafe_SDK/threeDSecure/CardinalSessionProtocol.swift
@@ -0,0 +1,25 @@
+//
+//  CardinalSessionProtocol.swift
+//  Paysafe_SDK
+//
+//  Created by Ivelin Davidov on 14.08.19.
+//  Copyright © 2019 Paysafe. All rights reserved.
+//
+
+import Foundation
+import CardinalMobile
+
+protocol CardinalSessionProtocol {
+    func configure(_ sessionConfig: CardinalSessionConfiguration)
+
+    func setup(jwtString: String,
+               completed didCompleteHandler: @escaping CardinalSessionSetupDidCompleteHandler,
+               validated didValidateHandler: @escaping CardinalSessionSetupDidValidateHandler)
+
+    func continueWith(transactionId: String,
+                      payload: String,
+                      validationDelegate: CardinalValidationDelegate)
+}
+
+extension CardinalSession: CardinalSessionProtocol {
+}
---
+++ Pods/Target Support Files/Paysafe_SDK/Paysafe_SDK-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Paysafe_SDK : NSObject
+@end
+@implementation PodsDummy_Paysafe_SDK
+@end
